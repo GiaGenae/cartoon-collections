e2343ce1880f8729ca1d2aad71d8a1d058b44155
Gia Jennings <gjennings.co@gmail.com>
2020-11-01 11:16:11 -0500

    commit tree 4e8b3272dc343a0e872e1eecc747ff4c4b743c8b

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,25 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(collection)
+  i = 0
+  collection.each_with_index {|name, i| puts "#{i + 1}. #{name}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(collection)
+  planeteer_calls = []
+  collection.collect do |calls|
+    planeteer_calls << "#{calls.capitalize}!"
+  end
+  planeteer_calls
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(collection)
+  collection.any? do |word|
+    word.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(collection)
+  cheese_types = ["cheddar", "gounda", "camembert"]
+  collection.find do |cheese|
+    cheese_types.include?(cheese)
+  end
 end
